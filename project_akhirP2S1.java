e0ea1cbfef7022572ad1d719b5bf8c0e6f58191a
randamopobela <98214133+randamopobela@users.noreply.github.com>
2022-02-10 03:49:12 +0900

    Project Akhir Introduction to Programming 2112
    Project Akhir Team 1 kelas JQBA

---
+++ project_akhirP2S1.java
@@ -0,0 +1,79 @@
+import javax.swing.*;
+import java.util.Arrays;
+
+public class Menu {
+
+    static int angka;
+    static int[] nilaiArray;
+
+    private JPanel MenuPanel;
+    private JTextField tfBilanganAcak;
+    private JButton acakAngkaButton;
+    private JButton sortingAscendingButton;
+    private JButton sortingDescendingButton;
+    private JButton keluarButton;
+    private JTextField tfAscending;
+    private JTextField tfDescending;
+    private JTextField tfAngka;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Menu");
+        frame.setContentPane(new Menu().MenuPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public Menu() {
+        acakAngkaButton.addActionListener(e -> {
+            angka = Integer.parseInt(tfAngka.getText());
+            nilaiArray = new int[angka];
+
+            for (int i = 0; i < nilaiArray.length; i++) {
+                int acak = (int) (Math.random() * 21);
+                if (acak % 2 == 0 && acak > 0) {
+                    nilaiArray[i] = acak;
+                }
+                else {
+                    i--;
+                }
+            }
+
+            tfBilanganAcak.setText(Arrays.toString(nilaiArray));
+            tfAscending.setText("");
+            tfDescending.setText("");
+
+        });
+        sortingAscendingButton.addActionListener(e -> {
+            tfAscending.setText("");
+            for(int i = 0; i < nilaiArray.length; i++) {
+                for(int j = i+1; j < nilaiArray.length; j++) {
+                    if(nilaiArray[i] > nilaiArray[j]) {
+                        int temp = nilaiArray[i];
+                        nilaiArray[i] = nilaiArray[j];
+                        nilaiArray[j] = temp;
+                    }
+                }
+            }
+
+            tfAscending.setText(Arrays.toString(nilaiArray));
+
+        });
+        sortingDescendingButton.addActionListener(e -> {
+            tfDescending.setText("");
+            for(int i = 0; i < nilaiArray.length; i++) {
+                for(int j = i+1; j < nilaiArray.length; j++) {
+                    if(nilaiArray[i] < nilaiArray[j]) {
+                        int temp = nilaiArray[i];
+                        nilaiArray[i] = nilaiArray[j];
+                        nilaiArray[j] = temp;
+                    }
+                }
+            }
+
+            tfDescending.setText(Arrays.toString(nilaiArray));
+
+        });
+        keluarButton.addActionListener(e -> System.exit(0));
+    }
+}
